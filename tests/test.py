47739215faa404c7049d4031bec1e707edfc3c24
Mehmet Çalışkan <49614175+alkanchosen@users.noreply.github.com>
2022-02-12 15:25:11 +0300

    - Added first test - Added ids to the components

---
+++ tests/test.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+s = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=s)
+driver.implicitly_wait(5)
+driver.maximize_window()
+driver.get('http://localhost:3000')
+driver.find_element(By.ID, 'mailOrPhone').send_keys('examplemail@gmail.com')
+driver.find_element(By.ID, 'password').send_keys('123456789')
+driver.find_element(By.ID, 'signInButton').click()
+assert driver.find_element(By.ID, 'notistack-snackbar').text == 'Login successful!'
