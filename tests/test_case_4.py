6a342c4a9b25d23a4ea9ec837bc73d55789c599e
atakan <atakanaray@gmail.com>
2022-02-21 21:53:07 +0300

    added test case 4
    writes null as password

---
+++ tests/test_case_4.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+# Test Case Description --> User logs in with correct e-mail and but inputs null as password
+s = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=s)
+driver.implicitly_wait(5)
+driver.maximize_window()
+driver.get('http://localhost:3000')
+driver.find_element(By.ID, 'mailOrPhone').send_keys('examplemail@gmail.com')
+driver.find_element(By.ID, 'password').send_keys('null')
+driver.find_element(By.ID, 'signInButton').click()
+assert driver.find_element(By.ID, 'notistack-snackbar').text == 'Login failed! Please check your credentials.'
